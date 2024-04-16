0a6ee54ca0b1f95a75c6360bdaeb2a88dce1ec22
Ady Ngom <adyngom@flywheel.io>
2024-04-16 15:46:23 -0400

    stable after button overhaul

--- feature/ui/src/index.ts
+++ feature/ui/src/index.ts
@@ -1,3 +1,9 @@
-export * from './lib/ui/ui.component';
 
 export * from './lib/ui/atoms/button/button.component';
+export * from './lib/ui/atoms/icon/icon.component';
+
+export * from './lib/ui/atoms/icon/icon.component';
+export * from './lib/ui/atoms/icon/icon-list/icon-list.component';
+export * from './lib/ui/atoms/icon/icon.token';
+
+export * from './lib/ui/atoms/cards/info-card/info-card.component';
