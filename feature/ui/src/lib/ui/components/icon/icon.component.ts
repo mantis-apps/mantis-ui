45801b8fa3173489c2fca8a44c14cbdcc3102c83
Ady Ngom <adyngom@flywheel.io>
2024-06-05 22:02:14 +0300

    A little cleanup on the Icon comonent

--- feature/ui/src/lib/ui/components/icon/icon.component.ts
+++ feature/ui/src/lib/ui/components/icon/icon.component.ts
@@ -1,30 +1,16 @@
 
-import { Component, Input, inject } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
 import { HlmIconComponent, IconSize } from '@spartan-ng/ui-icon-helm';
 import { provideIcons, IconName } from '@ng-icons/core';
-import { lucideMail, lucideUser2, lucideUsers2, lucideActivity, lucideTruck, lucideTrain } from '@ng-icons/lucide';
-import { ICONS } from './icon.token';
+import { lucideShieldQuestion } from '@ng-icons/lucide';
 
 @Component({
   selector: 'Icon',
   standalone: true,
   imports: [HlmIconComponent],
   providers: [
-    {
-      provide: ICONS,
-      useValue: {
-        lucideMail: 'lucideMail',
-        lucideUser2: 'lucideUser2',
-        lucideUsers2: 'lucideUsers2',
-      }
-    },
     provideIcons({
-      lucideMail,
-      lucideUser2,
-      lucideUsers2,
-      lucideActivity,
-      lucideTruck,
-      lucideTrain,
+      lucideShieldQuestion
     }),
   ],
   template: `
@@ -32,52 +18,15 @@ import { ICONS } from './icon.token';
       [name]="name"
       [size]="size"
       [color]="color"
-      [strokeWidth]="strokeWidth"
+      [strokeWidth]="strokeWidth ? strokeWidth() : undefined"
       [class]="class"></hlm-icon>
   `,
 })
 export class IconComponent {
-  private icons: Record<string, IconName> = inject(ICONS);
-  @Input({ required: true }) name!: IconName; // Required
+  @Input({ required: true }) name: IconName = 'lucideShieldQuestion';
   @Input() size: IconSize = 'none';
   @Input() color?: string;
-  @Input() strokeWidth?: string | number;
+  @Input() strokeWidth? = signal<string| number | undefined >(undefined);
   @Input() class?: string;
 }
 
-// import { Component, Input, OnInit } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
-// import { provideIcons, IconName } from '@ng-icons/core';
-// import * as lucideIcons from '@ng-icons/lucide';
-
-// @Component({
-//   selector: 'mantis-icon',
-//   standalone: true,
-//   imports: [CommonModule, HlmIconComponent],
-//   template: `
-//     <hlm-icon
-//       *ngIf="icon"
-//       [size]="size"
-//       [color]="color"
-//       [strokeWidth]="strokeWidth"
-//       [name]="iconName"
-//     >
-//     </hlm-icon>
-//   `,
-// })
-// export class IconComponent implements OnInit {
-//   @Input() iconName!: string;
-//   @Input() size: 'xs' | 'sm' | 'base' | 'lg' | 'xl' | 'none' = 'base';
-//   @Input() color: string | undefined = undefined;
-//   @Input() strokeWidth: string | number | undefined = undefined;
-
-//   icon: typeof lucideIcons[keyof typeof lucideIcons] | undefined;
-
-//   async ngOnInit() {
-//     if (this.iconName) {
-//       this.icon = lucideIcons[this.iconName as keyof typeof lucideIcons];
-//       provideIcons({ [this.iconName]: this.icon });
-//     }
-//   }
-// }
